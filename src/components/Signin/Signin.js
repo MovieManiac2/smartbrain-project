339df0fbf2c404bd602496958c1b0b2e02fbb3e7
MovieManiac2 <ryluz03@hotmail.com>
2022-12-12 13:38:34 -0300

    Connecting Front-End with Back-End

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
 
   onSubmitSignIn = (e) => {
     e.preventDefault();
-    fetch("http://localhost:3000/signin", {
+    fetch("https://inquisitive-faun-f67606.netlify.app/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
